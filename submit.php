f0b9096578aa5b9fd28fd6a4999b4c775782ece8
peasizedsunflower <mtlabiola@gmail.com>
2019-09-07 23:03:45 -0600

    Made changes and added php

---
+++ submit.php
@@ -0,0 +1,16 @@
+<?php
+
+$name=stripslashes($_POST['name']);
+$email=stripslashes($_POST['email']);
+$title=stripslashes($_POST['title']);
+$message=stripslashes($_POST['message']);
+
+$concat_msg="\nName: " . $name . "| Email: " . $email . "| Title: " . $title . "| Message: " . $message;
+
+$f=fopen('messages.txt', "a");
+fwrite($f, $concat_msg);
+
+fclose($f);
+
+echo "<script type='text/javascript'>alert('Thanks for contacting me! I'll get back to you soon')</script>";
+echo "<script type='text/javascript'>document.location='index.html'</script>";
